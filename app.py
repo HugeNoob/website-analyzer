124d5cca55dceea48bbb0865206ebfe81eadbd3c
Lingxi Xing <thelastoreointhejar@gmail.com>
2023-11-04 20:35:07 +0800

    refactor: Move logic to utils file

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
-from bs4 import BeautifulSoup
-
-from collections import defaultdict
-
-from dotenv import load_dotenv
-
 from flask import Flask, request, jsonify
-
-from urllib.parse import urlparse
 
 import requests
 
-from re import match
-
-from os import getenv
-
-from socket import gethostbyname, gaierror
+from utils import extract_domain_info, extract_subdomains, extract_asset_domains
 
 app = Flask(__name__)
-
-load_dotenv()
 
 
 @app.route('/')
@@ -26,17 +12,10 @@ def analyze_website():
     url = request.args.get('url')
 
     try:
-        response = requests.get(url)
-        response.raise_for_status()  # Check for HTTP request errors
-        html_content = response.text
-
-        # Parse HTML content using BeautifulSoup
-        soup = BeautifulSoup(html_content, 'html.parser')
-
         # Extract information
         domain_info = extract_domain_info(url)
         subdomains = extract_subdomains(url)
-        asset_domains = extract_asset_domains(soup)
+        asset_domains = extract_asset_domains(url)
 
         # Return the analysis results as JSON
         return jsonify({
@@ -47,106 +26,6 @@ def analyze_website():
 
     except requests.exceptions.RequestException as e:
         return jsonify({"error": "Failed to fetch the website data."})
-
-
-def extract_domain_info(url):
-    domain = urlparse(url).netloc
-
-    try:
-        ip = gethostbyname(domain)
-    except gaierror:
-        ip = None
-        print("Failed to get IP address.")
-
-    if ip:
-        location_data = get_ip_location_data(ip)
-    else:
-        location_data = defaultdict(lambda: "Failed to get location data")
-
-    return {
-        "ip": ip,
-        "isp": location_data["isp"],
-        "organization": location_data["organization"],
-        "asn": location_data["asn"],
-        "location": location_data["location"],
-    }
-
-
-def get_ip_location_data(ip):
-    IP_API_KEY = getenv("IP_API_KEY")
-    response = requests.get(
-        f'https://api.ipgeolocation.io/ipgeo?apiKey={IP_API_KEY}&ip={ip}').json()
-
-    # The API above does not provide asn in response, despite mentioning it in documentation
-    # Hence, a different endpoint is needed for asn specifically
-    asn = requests.get(f'https://ipapi.co/{ip}/json/').json().get("asn")
-
-    location_data = {
-        "isp": response.get("isp"),
-        "organization": response.get("organization"),
-        "asn": asn,
-        "location": response.get("country_code2"),
-    }
-    return location_data
-
-
-def extract_subdomains(url):
-    parts = urlparse(url).netloc.split('.')
-    domain = '.'.join(parts[-2:])
-
-    SUBDOMAIN_API_KEY = getenv("SUBDOMAIN_API_KEY")
-    response = requests.get(
-        f'https://subdomains.whoisxmlapi.com/api/v1?apiKey={SUBDOMAIN_API_KEY}&domainName={domain}').json()
-
-    subdomains = []
-    for record in response.get("result").get("records"):
-        subdomains.append(record["domain"])
-
-    return subdomains
-
-
-def get_domain(url):
-    return urlparse(url).netloc
-
-
-def extract_asset_domains(soup):
-    javascripts = set()
-    for script in soup.find_all('script', src=True):
-        src = script['src']
-        if src and match(r'^https?://', src):
-            javascripts.add(get_domain(src))
-
-    stylesheets = set()
-    for link in soup.find_all('link', rel='stylesheet'):
-        href = link.get('href')
-        if href and match(r'^https?://', href):
-            stylesheets.add(get_domain(href))
-
-    images = set()
-    for img in soup.find_all('img', src=True):
-        src = img['src']
-        if src and match(r'^https?://', src):
-            images.add(get_domain(src))
-
-    iframes = set()
-    for iframe in soup.find_all('iframe', src=True):
-        src = iframe['src']
-        if src and match(r'^https?://', src):
-            iframes.add(get_domain(src))
-
-    anchors = set()
-    for anchor in soup.find_all('a', href=True):
-        href = anchor['href']
-        if href and match(r'^https?://', href):
-            anchors.add(get_domain(href))
-
-    return {
-        "javascripts": list(javascripts),
-        "stylesheets": list(stylesheets),
-        "images": list(images),
-        "iframes": list(iframes),
-        "anchors": list(anchors)
-    }
 
 
 if __name__ == '__main__':
